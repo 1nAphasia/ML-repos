77971300cb442498d315530a1b6c4345ec856f6c
Vista <792372447@qq.com>
2025-04-25 13:20:30 +0800

    4/25 13:20

--- Python/Dive into deep learning/Chap2/SoftMaxReg.py
+++ Python/Dive into deep learning/Chap2/SoftMaxReg.py
@@ -1,62 +1,44 @@
 import torch
-import torchvision
-from torch.utils import data
-from torchvision import transforms
+from IPython import display
 from d2l import torch as d2l
 
-d2l.use_svg_display()
-
-trans = transforms.ToTensor()
-mnist_train = torchvision.datasets.FashionMNIST(
-    root="../data", train=True, transform=trans, download=True
-)
-
-mnist_test = torchvision.datasets.FashionMNIST(
-    root="../data", train=False, transform=trans
-)
-
-
-def get_fashion_mnist_labels(lebels):
-    text_labels = [
-        "t-shirt",
-        "trouser",
-        "pullover",
-        "dress",
-        "coat",
-        "sandal",
-        "shirt",
-        "sneaker",
-        "bag",
-        "ankle boot",
-    ]
-    return [text_labels[int(i)] for i in lebels]
-
-
-def show_images(img, num_rows, num_cols, scale=1.5):
-    figsize = (num_cols * scale, num_rows * scale)
-    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
-    axes = axes.flatten()
-    for i, (ax, img) in enumerate(zip(axes, img)):
-        ax.imshow(img.numpy())
-    else:
-        ax.imshow(img)
-
-
-X, y = next(iter(data.DataLoader(mnist_train, batch_size=18, shuffle=True)))
-
-show_images(X.reshape(18, 1, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
-
 batch_size = 256
+trainIter, testIter = d2l.load_data_fashion_mnist(batch_size)
+
+num_inputs = 784
+num_outputs = 10
+
+W = torch.normal(0, 0.01, (num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+X = torch.tensor([[1, 2, 3], [4, 5, 6]])
 
 
-def get_dataloader_workers():
-    return 4
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition  # Broadcasting
 
 
-train_iter = data.DataLoader(
-    mnist_train, batch_size, shuffle=True, num_workers=get_dataloader_workers()
-)
-timer = d2l.Timer()
-for X, t in train_iter:
-    continue
-print(f"timer.stop():.2f:{timer.stop():.2f} sec")
+X = torch.normal(0, 1, (2, 5))
+X_prob = softmax(X)
+X_prob, X_prob.sum(1)
+
+
+def net(X):
+    return softmax(torch.matmul(X.reshape((-1, num_inputs)), W) + b)
+
+
+y = torch.tensor([0, 2])
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+
+
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[range(len(y_hat)), y])
+
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
