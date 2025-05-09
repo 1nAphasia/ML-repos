723341a639061ad0c00907b8ce279f0dce2af5fa
Vista <792372447@qq.com>
2025-05-09 21:42:17 +0800

    123

---
+++ Python/Dive into deep learning/Chap8/MLC.py
@@ -0,0 +1,9 @@
+import random
+import torch
+from d2l import torch as d2l
+
+tokens = d2l.tokenize(d2l.read_time_machine())
+
+corpus = [token for line in tokens for token in line]
+vocab = d2l.Vocab(corpus)
+print(f"vocab.token_freqs[:10]:{vocab.token_freqs[:10]}")
